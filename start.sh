51f24fa4800b51d52f5b5b0d9cb9058b8d2d7d22
Benjamin Klum <bklum@avantgarde-labs.de>
2017-03-15 00:05:59 +0100

    Resolve proxy backend dynamically

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Lookup nameserver for dynamic proxy backend lookup
+export NAMESERVER=`cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' '`
+
 confd -onetime -backend env
 exec nginx -g "daemon off;"
